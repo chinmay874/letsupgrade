9e77435ce1797ef56fa38bb855364889b0028d7c
chinmay jain <43497155+chinmay874@users.noreply.github.com>
2020-07-23 00:55:38 +0530

    Add files via upload

---
+++ Python Essentials _ Day 1 Assignment.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Question 1. What is Python ? What are the ways to install python in your PC ?
+Solution :
+* Python is an interpreted, object-oriented, high-level programming language , Developed by Guido van Rossum , first released in 1991 but got famous in 2006 .
+Python supports modules and packages . It has huge amount of libraries to make our code simpler and shorter , easier to read .
+
+* Ways to install Python in PC :
+1. From Python Official site :
+- Visit www.python.org
+- Click Download option
+- There are list of Python releases by version number , Choose the version which you want to install .
+- Install it according to your Pc's OS
+2. From Anaconda Official Site :
+- Visit www.anaconda.com
+- Click on Products Dropdown Menu and Select Individual Edition
+- Click Download option and Choose your appropriate OS
+- Install it
+
+Question 2 : What are Code Environments ? What are different Types ?
+Solution :
+* A code environment is a standalone and self-contained environment in which you can Write code , Compile code and see the Corresponding Output
+
+* Types :
+1. ) Text Editors
+example -- Atom , Visual Studio Code
+2. ) Full IDE'S
+example -- Pycharm , Spyder
+3. ) Notebook Environment
+example -- Generally used Jupyter Notebooks
+
+
+# In[ ]:
+
+
+
+
